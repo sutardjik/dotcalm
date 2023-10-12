fc50d5003de7d0ba88c3a7aed364316c2a402833
sutardjik <sealion19@yahoo.com>
2023-10-12 21:51:28 +0800

    add pics

--- src/pages/components/Gallery.js
+++ src/pages/components/Gallery.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
 import Lightbox from './Lightbox';
+import sydney from "../../img/sydney.png";
+import folktalespirits from "../../img/folktalespirits.png";
+import gontrancherrier from "../../img/gontrancherrier.png";
+import ramune from "../../img/ramune.png";
+import cd from "../../img/cd.png";
+import isseymiyake from "../../img/isseymiyake.png";
 import zeus from "../../img/zeus.png";
+import takashimurakami from "../../img/takashimurakami.png";
 import cafe from "../../img/cafe.png";
+import burberry from "../../img/burberry.png";
 import dine from "../../img/dine.png";
-import burberry from "../../img/burberry.png"
-import outfits from "../../img/outfits.png"
-import potions from "../../img/potions.png"
-import streetwear from "../../img/streetwear.png"
-import gentlemonster from "../../img/gentlemonster.png"
-import art from "../../img/art.png"
-import air from "../../img/air.png"
-import airplane from "../../img/airplane.png"
-import mountain from "../../img/mountain.png"
-import park from "../../img/park.png"
-import classical from "../../img/classical.png"
-import aerialtram from "../../img/aerialtram.png"
+import outfits from "../../img/outfits.png";
+import art from "../../img/art.png";
+import potions from "../../img/potions.png";
+import streetwear from "../../img/streetwear.png";
+import gentlemonster from "../../img/gentlemonster.png";
+import air from "../../img/air.png";
+import airplane from "../../img/airplane.png";
+import mountain from "../../img/mountain.png";
+import park from "../../img/park.png";
+import classical from "../../img/classical.png";
+import aerialtram from "../../img/aerialtram.png";
 
 const Gallery = () => {
   const [lightboxImage, setLightboxImage] = useState(null);
@@ -28,15 +35,22 @@ const Gallery = () => {
   };
 
   const images = [
+    { src: sydney, alt: "" },
+    { src: folktalespirits, alt: "" },
+    { src: gontrancherrier, alt: "" },
+    { src: ramune, alt: "" },
+    { src: cd, alt: "" },
+    { src: isseymiyake, alt: "" },
     { src: zeus, alt: "" },
+    { src: takashimurakami, alt: "" },
     { src: cafe, alt: "" },
-    { src: dine, alt: "" },
     { src: burberry, alt: "" },
+    { src: dine, alt: "" },
     { src: outfits, alt: "" },
     { src: potions, alt: "" },
     { src: streetwear, alt: "" },
-    { src: gentlemonster, alt: "" },
     { src: art, alt: "" },
+    { src: gentlemonster, alt: "" },
     { src: air, alt: "" },
     { src: airplane, alt: "" },
     { src: mountain, alt: "" },
