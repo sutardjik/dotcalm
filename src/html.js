25c9b56b04f2e94e8d50fa76e04a6595675b9144
sutardjik <sealion19@yahoo.com>
2024-01-08 10:23:19 +0800

    remove icon link

--- src/html.js
+++ src/html.js
@@ -7,7 +7,6 @@ export default function HTML(props) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="robots" content="noindex, nofollow" />
-        <link rel="icon" href="../img/favicon.ico" />
         {props.headComponents}
       </head>
       <body>
